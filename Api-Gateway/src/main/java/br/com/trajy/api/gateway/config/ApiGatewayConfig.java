18e65ddc9db717271e09b3df3faa630536434c25
Trajy <henrique.trajano.a@gmail.com>
2023-03-13 20:44:23 -0300

    Create ApiGatewayConfig.java

---
+++ Api-Gateway/src/main/java/br/com/trajy/api/gateway/config/ApiGatewayConfig.java
@@ -0,0 +1,25 @@
+package br.com.trajy.api.gateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        Function<PredicateSpec, Buildable<Route>> function = predicateSpec -> {
+            return predicateSpec.path("/get").uri("http://httpbin.org:80");
+        };
+        return builder.routes()
+                .route(function)
+                .build();
+    }
+
+}
