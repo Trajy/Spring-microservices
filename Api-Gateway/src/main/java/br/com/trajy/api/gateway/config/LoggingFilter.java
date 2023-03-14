32005631c9d7b750b6e09faf02f93ae8bd692978
Trajy <henrique.trajano.a@gmail.com>
2023-03-14 20:01:41 -0300

    added-filter-in-api-geteway

---
+++ Api-Gateway/src/main/java/br/com/trajy/api/gateway/config/LoggingFilter.java
@@ -0,0 +1,20 @@
+package br.com.trajy.api.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Original Request Path: {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+}
